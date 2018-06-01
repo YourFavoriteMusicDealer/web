2b84c5aeea2d51e3fc6041db53cee569c49c2b54
Prischenko Sergey <jonkofee@icloud.com>
2018-06-01 12:22:28 +0300

    :zap: support config file

---
+++ src/config/example.js
@@ -0,0 +1,4 @@
+//Create index.js
+export default {
+  gaKey: ''
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Icon from 'vue-svg-icon/Icon.vue'
+import config from './config'
+
+console.log(config)
 
 Vue.component('icon', Icon)
 
