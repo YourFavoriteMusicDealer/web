79d092821f2bb15e527a6f0b69bc248b1355c536
Виталий Чернявский <4irik91@mail.ru>
2018-06-05 20:27:36 +0300

    #36 Роутинг

--- src/main.js
+++ src/main.js
@@ -2,10 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import Icon from 'vue-svg-icon/Icon.vue'
 import config from './config'
+import VueRouter from 'vue-router'
+import Player from './pages/Player.vue'
+import Home from './pages/Home.vue'
+
+const Bar = { template: '<div>bar</div>' }
+
+Vue.use(VueRouter)
 
 Vue.component('icon', Icon)
 
+const router = new VueRouter({
+  routes: [
+    { path: '/home', component: Home },
+    { path: '/bar', component: Bar },
+    { path: '/:telegrmMassageId', component: Player }
+  ] 
+})
+
 new Vue({
   el:     '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
