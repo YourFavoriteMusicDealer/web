38b2ef20d21080fee6f1768d9ff9dec9f5913ebb
Виталий Чернявский <4irik91@mail.ru>
2018-06-06 14:56:05 +0300

    #36 Поместил  Vue.use(VueRouter) в pages-router.jss

--- src/pages-router.js
+++ src/pages-router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Player from './pages/Player.vue'
 
+Vue.use(VueRouter)
+
 const router = new VueRouter({
   mode:   'history',
   routes: [
