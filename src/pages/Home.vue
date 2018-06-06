7ea1c053dd48429cd0a99f9e34d6f4542792994e
Виталий Чернявский <4irik91@mail.ru>
2018-06-06 16:24:54 +0300

    #36 Домашняя страница

---
+++ src/pages/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ welcomeMessage }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      welcomeMessage: 'Здравствуй,странник!=)'
+    }
+  }
+}
+</script>
+
+<style>
+
+div {
+  text-align: center;
+  color: #ff2d55;
+  font-size: 30px;
+}
+
+</style>
