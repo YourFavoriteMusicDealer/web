9acce80c6b136d123af77a26edbab828502f5f3a
Виталий Чернявский <4irik91@mail.ru>
2018-06-06 13:41:18 +0300

    #36 изменил main.js для роутинго

--- src/main.js
+++ src/main.js
@@ -2,24 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import Icon from 'vue-svg-icon/Icon.vue'
 import config from './config'
+import router from './pages-router.js'
 import VueRouter from 'vue-router'
-import Player from './pages/Player.vue'
-import Home from './pages/Home.vue'
-
-const Bar = { template: '<div>bar</div>' }
 
 Vue.use(VueRouter)
 
 Vue.component('icon', Icon)
-
-const router = new VueRouter({
-  mode:   'history',
-  routes: [
-    { path: '/home', component: Home },
-    { path: '/bar', component: Bar },
-    { path: '/:telegrmMassageId', component: Player }
-  ] 
-})
 
 new Vue({
   el:     '#app',
