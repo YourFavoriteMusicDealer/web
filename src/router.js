f38b4566e5e2702a9834175cbe3cad178fff83b1
Виталий Чернявский <4irik91@mail.ru>
2018-06-07 19:41:43 +0300

    #36 Переименовал страницу NotFound  и переменные роутера

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from './pages/Home.vue'
-import Player from './pages/Player.vue'
-import NotFoundComponent  from './pages/404.vue'
+import HomeComponent from './pages/Home.vue'
+import PlayerComponent from './pages/Player.vue'
+import NotFoundComponent  from './pages/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -10,8 +10,8 @@ const router = new VueRouter({
   mode:   'history',
   routes: [
     { path: '*', component: NotFoundComponent },
-    { path: '/', component: Home },
-    { path: '/:telegramMessageId', component: Player, props: true }
+    { path: '/', component: HomeComponent },
+    { path: '/:telegramMessageId', component: PlayerComponent, props: true }
   ] 
 })
 
