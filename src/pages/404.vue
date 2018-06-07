24b4fe7e8ae651dcdcd29c2034f1031e813c72d6
Виталий Чернявский <4irik91@mail.ru>
2018-06-07 19:19:47 +0300

    Добавил notFoundComponent в случае несовпадения url

---
+++ src/pages/404.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ notFoundComponent }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      notFoundComponent: '404'
+    }
+  }
+}
+</script>
+
+<style>
+
+div {
+  text-align: center;
+  color: #ff2d55;
+  font-size: 100px;
+}
+
+</style>
